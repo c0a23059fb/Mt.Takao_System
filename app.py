e974577b94cf3ff7553ae1ff43ec383862e2131c
c0a2303678 <c0a2303678@edu.teu.ac.jp>
2025-05-31 15:31:50 +0900

    gitignoreの設定及び、古い秘密鍵の削除

--- app.py
+++ app.py
@@ -6,12 +6,21 @@ import cv2
 import numpy as np
 import pyzbar.pyzbar as pyzbar
 import os
+import ssl
+from dotenv import load_dotenv
 
 app = Flask(
     __name__,
     static_folder='Front/static',
     template_folder='Front/templates'
 )
+
+load_dotenv()
+cert_path = os.path.join(os.path.dirname(__file__), 'keys', 'new_cert.pem')  # new_cert.pem の絶対パス
+key_path = os.path.join(os.path.dirname(__file__), 'keys', 'new_key.pem')   # new_key.pem の絶対パス
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(cert_path, key_path)
 
 @app.route('/')
 def home():
@@ -29,6 +38,7 @@ def shop():
 def scan_qr_code():
     try:
         data = request.json['image']
+        print(f"Received data: {data[:100]}")  # デバッグ用ログ（データの先頭100文字を表示）
         image_data = base64.b64decode(data.split(',')[1])
         image = Image.open(BytesIO(image_data)).convert('RGB')
         image_np = np.array(image)
@@ -42,7 +52,10 @@ def scan_qr_code():
         else:
             return jsonify({"success": False, "error": "No QR code detected"})
     except Exception as e:
+        print(f"Error: {e}")  # デバッグ用ログ
         return jsonify({"success": False, "error": str(e)})
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # 現在のファイルのディレクトリを取得
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='0.0.0.0', port=8000, ssl_context=context)
